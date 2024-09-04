a7fc122853bc19236d5a80f29060fc6e1410818b
Potato <44370952+P0tatoooo@users.noreply.github.com>
2024-09-05 00:47:31 +0200

    Update autoheal

--- server/events.lua
+++ server/events.lua
@@ -117,16 +117,18 @@ RegisterNetEvent('QBCore:UpdatePlayer', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-    local newHunger = Player.PlayerData.metadata['hunger'] - QBCore.Config.Player.HungerRate
-    local newThirst = Player.PlayerData.metadata['thirst'] - QBCore.Config.Player.ThirstRate
-    if newHunger <= 0 then
-        newHunger = 0
+    if not Player.PlayerData.metadata.autoheal then
+        local newHunger = Player.PlayerData.metadata['hunger'] - QBCore.Config.Player.HungerRate
+        local newThirst = Player.PlayerData.metadata['thirst'] - QBCore.Config.Player.ThirstRate
+        if newHunger <= 0 then
+            newHunger = 0
+        end
+        if newThirst <= 0 then
+            newThirst = 0
+        end
+        Player.Functions.SetMetaData('thirst', newThirst)
+        Player.Functions.SetMetaData('hunger', newHunger)
     end
-    if newThirst <= 0 then
-        newThirst = 0
-    end
-    Player.Functions.SetMetaData('thirst', newThirst)
-    Player.Functions.SetMetaData('hunger', newHunger)
     TriggerClientEvent('hud:client:UpdateNeeds', src, newHunger, newThirst)
     Player.Functions.Save()
 end)
