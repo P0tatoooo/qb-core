242accc58a23466f30e7af2baf332964503022b5
Potato <44370952+P0tatoooo@users.noreply.github.com>
2024-10-24 17:32:55 +0200

    Add wait before check for food

--- client/loops.lua
+++ client/loops.lua
@@ -10,6 +10,9 @@ CreateThread(function()
 end)
 
 CreateThread(function()
+    while not LocalPlayer.state.isLoggedIn do Citizen.Wait(500) end
+    Citizen.Wait(20000)
+    
     while true do
         if LocalPlayer.state.isLoggedIn then
             if (QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0) and not (QBCore.PlayerData.metadata['isdead'] or QBCore.PlayerData.metadata['inlaststand']) then
