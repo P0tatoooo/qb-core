0e3d6dfb9fa5a6c0ab6d25b43ae863f5b00f40e8
Potato <44370952+P0tatoooo@users.noreply.github.com>
2025-03-11 17:02:32 +0100

    Update latent

--- client/main.lua
+++ client/main.lua
@@ -11,7 +11,7 @@ end)
 
 Citizen.CreateThread(function()
     Citizen.Wait(5000)
-    QBCore.Functions.TriggerCallback('QBCore:GetServerGangs', function(gangs, jobs)
+    QBCore.Functions.TriggerLatentCallback('QBCore:GetServerGangs', function(gangs, jobs)
         QBCore.Shared.Gangs = gangs
         QBCore.Shared.Jobs = jobs
         TriggerEvent("QBCore:Client:UpdateObject")
