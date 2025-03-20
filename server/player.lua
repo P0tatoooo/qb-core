89de5b286fdacf006d2b87a5b482f881cefbeb73
Potato <44370952+P0tatoooo@users.noreply.github.com>
2025-03-20 16:05:00 +0100

    Fix wipe log

--- server/player.lua
+++ server/player.lua
@@ -723,7 +723,7 @@ function QBCore.Player.ForceDeleteCharacter(citizenid, sourceplayer)
         local result = MySQL.query.await('INSERT INTO old_storages SELECT * FROM storages WHERE owner = @citizenid', {['@citizenid'] = citizenid})
         local result = MySQL.query.await('DELETE FROM storages WHERE owner = @citizenid', {['@citizenid'] = citizenid})
 
-        local message = 'Nom : **' .. Player.PlayerData.rpname .. '**\nJob : **' .. Player.PlayerData.gang.label .. '**\nFaction : **' .. Player.PlayerData.gang.label .. '**\nCitizenId : **' .. Player.PlayerData.citizenid .. '**\nLicense : **' .. Player.PlayerData.license .. '**'
+        local message = 'Nom : **' .. Player.PlayerData.rpname .. '**\nJob : **' .. Player.PlayerData.job.label .. '**\nFaction : **' .. Player.PlayerData.gang.label .. '**\nCitizenId : **' .. Player.PlayerData.citizenid .. '**\nLicense : **' .. Player.PlayerData.license .. '**'
         TriggerEvent('MyCity_Core:Wipe:Logs', "Wipe", message, sourceplayer)
     end
 end
